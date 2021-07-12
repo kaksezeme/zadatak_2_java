df98d7149f0969657b633d29d77426349681fc8a
kaksezeme <pvlahovic@outlook.com>
2021-07-12 12:34:20 +0200

    Update Main for merging conflicts

--- src/main/java/hr/king/akademija/generator/BasicPasswordGenerator.java
+++ src/main/java/hr/king/akademija/generator/BasicPasswordGenerator.java
@@ -5,7 +5,8 @@ public class BasicPasswordGenerator extends PasswordGenerator {
 
     @Override
     public String generatePassword(int size) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        //this is bad exception message. Fixed it
+        throw new UnsupportedOperationException("TODO");
     }
 
 }
