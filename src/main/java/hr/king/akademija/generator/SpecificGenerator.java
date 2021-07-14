9ed11028b84db6f522e2bb7f248c66e4b2f18dec
Patrik Galina <pgalina@foi.hr>
2021-07-14 12:53:44 +0200

    Reimplementirana metoda SpecificGenerator[generiranje passworda s isključivo malim slovima bez brojki]

--- src/main/java/hr/king/akademija/generator/SpecificGenerator.java
+++ src/main/java/hr/king/akademija/generator/SpecificGenerator.java
@@ -4,14 +4,26 @@ import java.util.Locale;
 
 public class SpecificGenerator extends PasswordGenerator {
 
+    private static final String LOWER_CASE_LETTTERS = "abcdefghijklmnopqrstuvwxyz" ;
+
     @Override
     public String generatePassword(int size) {
         String password = "";
 
         for(int i=0; i<size; i++) {
-            password += Character.toString(getRandomCharacter()).toLowerCase();
+            password += generateAllowedChar();
         }
 
         return password;
+    }
+
+    private String generateAllowedChar() {
+        String generatedChar;
+
+        do{
+            generatedChar = Character.toString(getRandomCharacter());
+        }while(LOWER_CASE_LETTTERS.indexOf(generatedChar) < 0);
+
+        return generatedChar.toLowerCase();
     }
 }
