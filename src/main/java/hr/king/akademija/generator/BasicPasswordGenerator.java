4c6a67db72c0d5e7689cce28b76d0dc4b4345f57
Patrik Galina <pgalina@foi.hr>
2021-07-14 12:12:08 +0200

    Implementirana metoda BasicPasswordGenerator

--- src/main/java/hr/king/akademija/generator/BasicPasswordGenerator.java
+++ src/main/java/hr/king/akademija/generator/BasicPasswordGenerator.java
@@ -5,7 +5,13 @@ public class BasicPasswordGenerator extends PasswordGenerator {
 
     @Override
     public String generatePassword(int size) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        String password = "";
+
+        for(int i=0; i<size; i++) {
+            password += Character.toString(getRandomCharacter());
+        }
+
+        return password;
     }
 
 }
