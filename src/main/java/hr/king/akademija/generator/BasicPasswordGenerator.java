6a3f6d1d20a7d13d766c86e15b26bf06d5357848
Mario Pernar <mariopernar5@gmail.com>
2021-07-14 12:12:58 +0200

    Implemented method generatePassword

--- src/main/java/hr/king/akademija/generator/BasicPasswordGenerator.java
+++ src/main/java/hr/king/akademija/generator/BasicPasswordGenerator.java
@@ -5,7 +5,12 @@ public class BasicPasswordGenerator extends PasswordGenerator {
 
     @Override
     public String generatePassword(int size) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        StringBuilder password = new StringBuilder();
+        for (int i = 0; i < size; i++){
+            password.append(getRandomCharacter());
+        }
+
+        return password.toString();
     }
 
 }
